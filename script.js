ecf5592055f8fc6a9647e1f065842955561d6a1e
Haruu315 <haruki6218@gmail.com>
2026-05-23 18:03:39 +0900

    グラフの描画方法を棒グラフに変更した

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function calc() {
     const labels        = []
     const totalData     = []
     const principalData = []
+    const profitData = []
 
     for (let month = 1; month <= years * 12; month++) {
         total = total * 1 + total * monthRate + monthly//複利の計算
@@ -19,6 +20,7 @@ function calc() {
             labels.push(year + "年")//グラフの横軸に年数を追加する
             totalData.push(Math.round(total))//そのときの資産総額を配列に追加。Math.round() で小数点以下を四捨五入
             principalData.push(monthly * 12 * year)//元本の累計を計算して追加。「毎月の積立額 × 12ヶ月 × 何年目か」
+            profitData.push(Math.round(total) - monthly * 12 * year)
         }
     }
 
@@ -31,13 +33,19 @@ function calc() {
 
     if (myChart) myChart.destroy()//「myChartに何か入っていたら古いグラフを消す」です。ボタンを2回押すと古いグラフの上に新しいグラフが重なってバグるので、先に消しています。最初は null なので1回目はスキップされます。
     myChart = new Chart(document.getElementById("chart"), {
-        type: "line",//グラフの種類
+        type: "bar",//グラフの種類
         data: {
             labels: labels,
             datasets: [
-                { label: "資産総額", data: totalData,     borderColor: "blue" },
-                { label: "元本",     data: principalData, borderColor: "gray" }
+                { label: "元本",     data: principalData, backgroundColor: "orange" },
+                { label: "資産総額", data: totalData,     backgroundColor: "steelblue" }
             ]
+        },
+        options: {
+            scales: {
+                x: { stacked: true},
+                y: { stacked: true}
+            }
         }
     })
 }
